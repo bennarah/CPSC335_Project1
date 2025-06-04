5cdab6efb79620d4abef07edb2d7a27a7fbd47a4
Bassma Ennarah <123920913+bennarah@users.noreply.github.com>
2025-06-04 21:06:50 +0000

    Counting Swaps

--- alternating_disc.py
+++ alternating_disc.py
@@ -1,23 +1,23 @@
 def sort_discs(discs):
+    swap_count = 0
     swapped = True
 
     while swapped:
         swapped = False
-
-        # Left to right pass
         for i in range(len(discs) - 1):
             if discs[i] == 'D' and discs[i + 1] == 'L':
                 discs[i], discs[i + 1] = discs[i + 1], discs[i]
                 swapped = True
+                swap_count += 1
 
-        # Right to left pass
         for i in range(len(discs) - 1, 0, -1):
             if discs[i - 1] == 'D' and discs[i] == 'L':
                 discs[i - 1], discs[i] = discs[i], discs[i - 1]
                 swapped = True
+                swap_count += 1
 
-    return discs
+    return discs, swap_count
 
-discs = ['L', 'D', 'L', 'D', 'D', 'L']
-sorted_discs = sort_discs(discs)
-print(sorted_discs)
+final_discs, swaps = sort_discs(['D', 'L', 'D', 'L'])
+print("Final order:", final_discs)
+print("Total swaps:", swaps)
