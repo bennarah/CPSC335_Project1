e7d8421f1e7e12c8184289b3ca9fe644e18640f1
Bassma Ennarah <123920913+bennarah@users.noreply.github.com>
2025-06-04 20:58:47 +0000

    1stDraft

---
+++ alternating_disc.py
@@ -0,0 +1,19 @@
+def sort_discs(discs):
+    swapped = True
+
+    while swapped:
+        swapped = False
+
+        # Left to right pass
+        for i in range(len(discs) - 1):
+            if discs[i] == 'D' and discs[i + 1] == 'L':
+                discs[i], discs[i + 1] = discs[i + 1], discs[i]
+                swapped = True
+
+        # Right to left pass
+        for i in range(len(discs) - 1, 0, -1):
+            if discs[i - 1] == 'D' and discs[i] == 'L':
+                discs[i - 1], discs[i] = discs[i], discs[i - 1]
+                swapped = True
+
+    return discs
